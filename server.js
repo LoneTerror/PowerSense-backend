ee6aa8078c430fd6039d65d04ff2d2ad9fd0019e
LoneTerror <nothingshere21@gmail.com>
2025-05-29 01:08:44 +0530

    Default Port Changed to 5001

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config(); // Load environment variables from .env file
 const sensorRoutes = require('./routes/sensor'); // Import your sensor routes
 
 const app = express();
-const PORT = process.env.PORT || 5000; // Use port from .env or default to 5000
+const PORT = process.env.PORT || 5001; // Use port from .env or default to 5000
 
 // Middleware
 app.use(cors()); // Enable CORS for all routes (adjust origins in production for security)
